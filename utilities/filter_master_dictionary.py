ba97195eaa3934c3906ea5628add73d12ee1fe17
Joshua Licudo <joshua.licudo.753@my.csun.edu>
2022-01-12 12:19:12 -0800

    Drop extra index, change years to flags in master dictionary

--- utilities/filter_master_dictionary.py
+++ utilities/filter_master_dictionary.py
@@ -4,6 +4,8 @@ dictionary = pandas.read_csv('LoughranMcDonald_MasterDictionary_2020.csv')
 
 condensed_dict = dictionary[(dictionary['Negative'] > 0) | (dictionary['Positive'] > 0)]
 condensed_dict = condensed_dict[['Word', 'Negative', 'Positive']]
+condensed_dict['Negative'].where(condensed_dict['Negative'] <= 0, 1, inplace=True)
+condensed_dict['Positive'].where(condensed_dict['Positive'] <= 0, 1, inplace=True)
 condensed_dict.reset_index(drop=True, inplace=True)
 
-condensed_dict.to_csv('LoughranMcDonald_MasterDictionary_2020__condensed.csv')
+condensed_dict.to_csv('LoughranMcDonald_MasterDictionary_2020__condensed.csv', index=False)
