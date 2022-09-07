5a46dd154855f7d6acae6ebd6436ef356cb573cb
Joshua Licudo <joshua.licudo.753@my.csun.edu>
2022-09-06 17:31:41 -0700

    Refactor price fetcher to update saved stock data

--- trader/price_fetcher.py
+++ trader/price_fetcher.py
@@ -14,18 +14,23 @@ class PriceFetcher:
         self.os = op_sys
 
     def get_price(self, ticker, time, start_date, end_date):
-        file = f'{self.stock_dir}/{ticker}.csv'
-        if not self.os.path.exists(file):
-            self.get_stock_history(ticker, start_date, end_date)
-        stock_prices = self.pandas.read_csv(file)
-        if 'Datetime' not in stock_prices.columns:
-            return -1
-        price = stock_prices.loc[stock_prices['Datetime'] == time]
-        if len(price) > 0:
-            return price['Close'].values[0]
-        else:
+        stock_history = self.get_stock_history(ticker, start_date, end_date)
+        if 'Datetime' not in stock_history.columns:
             return -1
 
+        price = stock_history.loc[stock_history['Datetime'] == time]
+        return price['Close'].values[0] if len(price) > 0 else -1
+
     def get_stock_history(self, ticker, start_date, end_date):
-        history = self.yfinance.Ticker(ticker).history( start=start_date, end=end_date, period='1d', interval='1m')
-        history.to_csv(f'{self.stock_dir}/{ticker}.csv')
+        history = self.yfinance.Ticker(ticker).history(start=start_date, end=end_date, period='1d', interval='1m')
+        file = f'{self.stock_dir}/{ticker}.csv'
+        if self.os.path.exists(file):
+            cached = self.pandas.read_csv(file, index_col=0, parse_dates=True)
+            history = self.merge_stock_data(history, cached)
+        history.to_csv(file)
+        return history
+
+    def merge_stock_data(self, new_data, old_data):
+        diff_data = self.pandas.concat([old_data, new_data])
+        diff_data = diff_data[~diff_data.index.duplicated(keep='last')]
+        return diff_data
--- trader/price_fetcher_tests.py
+++ trader/price_fetcher_tests.py
@@ -6,48 +6,68 @@ import pandas
 
 class PriceFetcherTests(unittest.TestCase):
     def test_get_price(self):
-        mock_os = Mock()
-        pf = PriceFetcher(op_sys=mock_os, stock_dir='mock_data/trader/prices')
+        pf = PriceFetcher(stock_dir='mock_data/trader/prices')
+        pf.get_stock_history = Mock()
+        pf.get_stock_history.return_value = pandas.read_csv('mock_data/trader/prices/GME.csv')
+        start_date, end_date = '2022-08-15', '2022-08-16'
+
+        price = pf.get_price('GME', '2022-08-15 10:00:00-04:00', start_date, end_date)
+        pf.get_stock_history.assert_called_with('GME', start_date, end_date)
+        self.assertEqual(price, 38.900001525878906)
+
+    def test_get_price_no_prices_returned(self):
+        mock_pandas = Mock()
+        pf = PriceFetcher(pd=mock_pandas, stock_dir='mock_data/trader/prices')
         pf.get_stock_history = Mock()
         start_date, end_date = '2022-08-15', '2022-08-16'
 
-        mock_os.path.exists.return_value = False
-        price = pf.get_price('GME', '2022-08-15 10:00:00-04:00', start_date, end_date)
-        self.assertEqual(price, 38.900001525878906)
-        pf.get_stock_history.assert_called_with('GME', start_date, end_date)
-
-        pf.get_stock_history.reset_mock()
-        mock_os.path.exists.return_value = True
-        pf.get_price('GME', '2022-08-15 10:00:00-04:00', start_date, end_date)
-        pf.get_stock_history.assert_not_called()
-
-    def test_get_price_no_prices_returned(self):
-        mock_os = Mock()
-        mock_pandas = Mock()
-        pf = PriceFetcher(op_sys=mock_os, pd=mock_pandas, stock_dir='mock_data/trader/prices')
-        start_date, end_date = '2022-08-15', '2022-08-16'
-
-        mock_os.path.exists.return_value = True
-        mock_pandas.read_csv.return_value = pandas.DataFrame(
+        pf.get_stock_history.return_value = pandas.DataFrame(
             columns=['Datetime', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
         price = pf.get_price('GME', '2022-08-15 10:00:00-04:00', start_date, end_date)
         self.assertEqual(-1, price)
 
-        mock_pandas.read_csv.return_value = pandas.DataFrame(
+        pf.get_stock_history.return_value = pandas.DataFrame(
             columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
         price = pf.get_price('GME', '2022-08-15 10:00:00-04:00', start_date, end_date)
         self.assertEqual(-1, price)
 
     def test_get_stock_history(self):
-        mock_yfinance = Mock()
-        mock_ticker = Mock()
+        mock_yfinance, mock_ticker, mock_history, mock_pandas, mock_os = Mock(), Mock(), Mock(), Mock(), Mock()
         mock_yfinance.Ticker.return_value = mock_ticker
-        mock_history = Mock()
         mock_ticker.history.return_value = mock_history
-        pf = PriceFetcher(yf=mock_yfinance)
+        pf = PriceFetcher(yf=mock_yfinance, pd=mock_pandas, op_sys=mock_os)
 
+        mock_os.path.exists.return_value = False
         start_date, end_date = '2022-08-15', '2022-08-16'
+        filename = 'intermediate_data/prices/MOCK.csv'
         pf.get_stock_history('MOCK', start_date, end_date)
-        mock_yfinance.Ticker.assert_called_with('MOCK')
         mock_ticker.history.assert_called_with(start=start_date, end=end_date, period='1d', interval='1m')
-        mock_history.to_csv.assert_called_with('intermediate_data/prices/MOCK.csv')
+        mock_pandas.read_csv.assert_not_called()
+        mock_pandas.concat.assert_not_called()
+        mock_history.to_csv.assert_called_with(filename)
+
+        mock_ticker.reset_mock()
+        mock_history.reset_mock()
+        mock_os.path.exists.return_value = True
+        mock_df = Mock()
+        mock_pandas.read_csv.return_value = mock_df
+        merge_df = Mock()
+        pf.merge_stock_data = Mock()
+        pf.merge_stock_data.return_value = merge_df
+        pf.get_stock_history('MOCK', start_date, end_date)
+        mock_ticker.history.assert_called_with(start=start_date, end=end_date, period='1d', interval='1m')
+        mock_pandas.read_csv.assert_called_with(filename, index_col=0, parse_dates=True)
+        pf.merge_stock_data.assert_called_with(mock_history, mock_df)
+        merge_df.to_csv.assert_called_with('intermediate_data/prices/MOCK.csv')
+
+    def test_merge_stock_data(self):
+        test_cases = ['all_difference', 'no_difference', 'more_old_than_new', 'more_new_than_old']
+        case_dir = 'mock_data/trader/price_fetcher/merge_stock_data'
+        pf = PriceFetcher()
+
+        for case in test_cases:
+            old_data = pandas.read_csv(f'{case_dir}/{case}/old.csv', index_col=0, parse_dates=True)
+            new_data = pandas.read_csv(f'{case_dir}/{case}/new.csv', index_col=0, parse_dates=True)
+            expected = pandas.read_csv(f'{case_dir}/expected.csv', index_col=0, parse_dates=True)
+            merge_data = pf.merge_stock_data(new_data, old_data)
+            pandas.testing.assert_frame_equal(merge_data, expected)
