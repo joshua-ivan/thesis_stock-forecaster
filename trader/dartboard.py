484a04b4b946fa7e7c80e5e2be9d332cddd03725
Joshua Licudo <joshua.licudo.753@my.csun.edu>
2022-10-01 16:46:35 -0700

    Dartboard closes positions w/same strat. as machine

--- trader/dartboard.py
+++ trader/dartboard.py
@@ -14,6 +14,7 @@ class DartboardInvestor:
                  rng=random, pf=PriceFetcher()):
         self.open_positions = []
         self.portfolio_value = 0.0
+        self.loss_threshold = 0.01
         self.start_date = start_date
         self.end_date = end_date
         self.tickers = tickers
@@ -23,20 +24,25 @@ class DartboardInvestor:
 
     def check_open_positions(self, closing_datetime):
         for i in range(len(self.open_positions) - 1, -1, -1):
-            if self.rng.randint(0, 99) < 50:
-                self.close_position(i, closing_datetime)
+            position = self.open_positions[i]
+            current_price = self.price_fetcher.get_price(position.ticker, closing_datetime,
+                                                         self.start_date, self.end_date)
 
-    def close_position(self, index, closing_datetime):
-        position = self.open_positions[index]
-        opening_value = position.price * position.quantity
-        current_price = self.price_fetcher.get_price(position.ticker, closing_datetime, self.start_date, self.end_date)
-        closing_value = current_price * position.quantity
+            opening_value = position.price * position.quantity
+            closing_value = current_price * position.quantity
+            raw_profit, profit_percent = 0.0, 0.0
+            if position.leverage_type == 'SHORT':
+                raw_profit = opening_value - closing_value
+                profit_percent = raw_profit / closing_value
+            elif position.leverage_type == 'LONG':
+                raw_profit = closing_value - opening_value
+                profit_percent = raw_profit / opening_value
 
-        if position.leverage_type == 'SHORT':
-            self.portfolio_value += (opening_value - closing_value)
-        elif position.leverage_type == 'LONG':
-            self.portfolio_value += (closing_value - opening_value)
+            if profit_percent > (1 * self.loss_threshold) or profit_percent < (-1 * self.loss_threshold):
+                self.close_position(raw_profit, i)
 
+    def close_position(self, raw_profit, index):
+        self.portfolio_value += raw_profit
         position = self.open_positions.pop(index)
 
         log_str = f'Closed position: {position}\nPortfolio value: {self.portfolio_value}'
--- trader/dartboard_tests.py
+++ trader/dartboard_tests.py
@@ -6,43 +6,45 @@ import pandas
 
 class DartboardTests(unittest.TestCase):
     def test_check_open_positions(self):
-        mock_rng = Mock()
-        dbi = DartboardInvestor(start_date='2022-08-15', end_date='2022-08-16', rng=mock_rng)
-        dbi.open_positions = [Mock()]
-        dbi.close_position = Mock()
-        rng_test_cases = [0, 49, 50, 99]
-        expected_calls = [
-            [call(0, '2022-08-15 10:00:00-04:00')],
-            [call(0, '2022-08-15 10:00:00-04:00')],
-            [],
-            [],
+        mock_fetcher = Mock()
+        mi = DartboardInvestor(start_date='2022-08-15', end_date='2022-08-16', pf=mock_fetcher)
+        mi.close_position = Mock()
+        mi.open_positions = [
+            Position('MOCK', 'SHORT', quantity=5, price=1000.00, date_time='2022-08-15 10:00:00-04:00'),
+            Position('MOCK', 'SHORT', quantity=5, price=1000.00, date_time='2022-08-15 11:00:00-04:00'),
+            Position('MOCK', 'SHORT', quantity=5, price=1000.00, date_time='2022-08-15 12:00:00-04:00'),
+            Position('MOCK', 'LONG', quantity=5, price=1000.00, date_time='2022-08-15 13:00:00-04:00'),
+            Position('MOCK', 'LONG', quantity=5, price=1000.00, date_time='2022-08-15 14:00:00-04:00'),
+            Position('MOCK', 'LONG', quantity=5, price=1000.00, date_time='2022-08-15 15:00:00-04:00')
         ]
+        mock_fetcher.get_price.side_effect = iter([2000.00, 1000.00, 500.00, 500.00, 1000.00, 2000.00])
 
-        for i in range(0, len(rng_test_cases)):
-            dbi.close_position.reset_mock()
-            mock_rng.randint.return_value = rng_test_cases[i]
-            dbi.check_open_positions('2022-08-15 10:00:00-04:00')
-            dbi.close_position.assert_has_calls(expected_calls[i])
-
-        dbi.open_positions = [Mock(), Mock(), Mock()]
-        mock_rng.randint.side_effect = iter([0, 0, 99])
-        dbi.check_open_positions('2022-08-15 10:00:00-04:00')
-        dbi.close_position.assert_has_calls(
-            [call(2, '2022-08-15 10:00:00-04:00'), call(1, '2022-08-15 10:00:00-04:00')])
+        mi.check_open_positions('2022-08-15 16:00:00-04:00')
+        mi.close_position.assert_has_calls([
+            call(5000, 5), call(-2500, 3), call(2500, 2), call(-5000, 0)
+        ])
 
     def test_close_position(self):
-        mock_price_fetcher = Mock()
-        dbi = DartboardInvestor(start_date='2022-08-15', end_date='2022-08-16', pf=mock_price_fetcher)
-        dbi.open_positions = [
+        mi = DartboardInvestor(start_date='2022-08-15', end_date='2022-08-16')
+        mi.open_positions = [
             Position('MOCK', 'SHORT', 5, 10000.00, '2022-08-15 10:00:00-04:00'),
             Position('MOCK', 'LONG', 5, 1000.00, '2022-08-15 11:00:00-04:00')
         ]
-        mock_price_fetcher.get_price.return_value = 100.00
 
-        dbi.close_position(0, '2022-08-15 11:00:00-04:00')
-        self.assertEqual(dbi.portfolio_value, 49500.00)
-        dbi.close_position(0, '2022-08-15 11:00:00-04:00')
-        self.assertEqual(dbi.portfolio_value, 45000.00)
+        expected_values = [49500.00, 45000.00]
+        for i in range(0, len(expected_values)):
+            opening_value = mi.open_positions[0].quantity * mi.open_positions[0].price
+            closing_value = mi.open_positions[0].quantity * 100.00
+            position = mi.open_positions[0]
+
+            raw_profit = 0.0
+            if position.leverage_type == 'SHORT':
+                raw_profit = opening_value - closing_value
+            elif position.leverage_type == 'LONG':
+                raw_profit = closing_value - opening_value
+
+            mi.close_position(raw_profit, 0)
+            self.assertEqual(mi.portfolio_value, expected_values[i])
 
     def test_new_open_position(self):
         mock_price_fetcher = Mock()
